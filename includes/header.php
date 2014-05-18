242cd66559b3fe9e915ed10a5eba1f918e57a78b
Joe Richardson <hello.joer@gmail.com>
2014-05-18 02:25:09 +0100

    Updated icons for app

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@
   <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/icon-114.png">
   <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/icon-72.png">
   <link rel="apple-touch-icon-precomposed" href="img/icon.png">
+  <link rel="SHORTCUT ICON" href="img/icon.png">
 
   <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700' rel='stylesheet' type='text/css'>
