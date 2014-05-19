ea08a275e5931c1bccc1006633f7077fb8780d77
Joe Richardson <hello.joer@gmail.com>
2014-05-19 13:30:49 +0100

    Added share icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
+
+    // scroll to top
     $(window).scroll(function() {
-        if($(this).scrollTop() > 100){
-            $('.toTop').stop().animate({
-                top: '0px'
-                }, 500);
-        }
-        else{
-            $('.toTop').stop().animate({
-               top: '-100px'
-            }, 500);
-        }
+      if($(this).scrollTop() > 100){
+        $('.toTop').stop().animate({
+          top: '0px'
+        }, 500);
+      }
+      else{
+        $('.toTop').stop().animate({
+         top: '-100px'
+       }, 500);
+      }
     });
     $('.toTop').click(function() {
-        $('html, body').stop().animate({
-           scrollTop: 0
-        }, 500, function() {
-           $('.toTop').stop().animate({
-               top: '-100px'
-           }, 500);
-        });
+      $('html, body').stop().animate({
+       scrollTop: 0
+     }, 500, function() {
+       $('.toTop').stop().animate({
+         top: '-100px'
+       }, 500);
+     });
     });
-});
+  });
+
