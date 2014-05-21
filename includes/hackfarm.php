4526a380104baf4b44d6f5b9da2a6508e80fe7a2
Joe Richardson <hello.joer@gmail.com>
2014-05-21 19:24:44 +0100

    stuff

--- includes/hackfarm.php
+++ includes/hackfarm.php
@@ -1,4 +1,20 @@
 <?php
+ // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+ 
+
   $rss = new DOMDocument();
   // pull in the feed
   $rss->load('http://feeds.feedburner.com/newsyc100?fmt=xml');
