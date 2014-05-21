57c02339c667021fcde2071addf22076b0f2da72
Joe Richardson <hello.joer@gmail.com>
2014-05-21 19:33:23 +0100

    made code prettier

--- includes/hackfarm.php
+++ includes/hackfarm.php
@@ -1,44 +1,43 @@
 <?php
-// Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  // allow from any origin
+  if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}
+
+  ");
   header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-      }
-    // Access-Control headers are received during OPTIONS requests
-      if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  header('Access-Control-Max-Age: 86400');
+  // cache for 1 day
+}
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-          header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-          header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-      }
-      $rss = new DOMDocument();
-  // pull in the feed
-      $rss->load('http://feeds.feedburner.com/newsyc100?fmt=xml');
-      $feed = array();
-      foreach ($rss->getElementsByTagName('item') as $node) {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}
 
-    // parse content
-        $item = array (
-          'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-          'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-          'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
-          );
-        array_push($feed, $item);
-      }
-      $limit = 25;
-      for($x=0;$x<$limit;$x++) {
+");
+}
 
-    // variables for pulling th stuff
-        $title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
-        $link = $feed[$x]['link'];
-        $date = date('jS M Y', strtotime($feed[$x]['date']));
+$rss = new DOMDocument();
+// pull in the feed
+$rss->load('http://feeds.feedburner.com/newsyc100?fmt=xml');
+$feed = array();
+foreach ($rss->getElementsByTagName('item') as $node) {
+  // parse content
+  $item = array (          'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,          'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,          'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,          );
+  array_push($feed, $item);
+}
 
-    // dump content
-        echo '<div class="post"><a href="'.$link.'" title="'.$title.'" target="_blank">'.$title.'</a>' . '<span class="read-btn">Read</span>';
-        echo '<div class="date">Posted on '.$date.'</div></div>';
-      }
-      ?>
+$limit = 25;
+for($x=0;$x<$limit;$x++) {
+  // variables for pulling th stuff
+  $title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+  $link = $feed[$x]['link'];
+  $date = date('jS M Y', strtotime($feed[$x]['date']));
+  // dump content
+  echo '<div class="post"><a href="'.$link.'" title="'.$title.'" target="_blank">'.$title.'</a>' . '<span class="read-btn">Read</span>';
+  echo '<div class="date">Posted on '.$date.'</div></div>';
+}
+
+?>
