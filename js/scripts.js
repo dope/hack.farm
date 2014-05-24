a312c0c225e2a44715cf11266fc539cd35fc7a20
Joe Richardson <hello.joer@gmail.com>
2014-05-25 00:53:49 +0100

    Added pop up menu

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,21 @@ $(document).ready(function() {
       $('.nav').toggleClass('slide-out');
       $('html').toggleClass('html-slide');
     });
+    $('.popup').click(function(event) {
+      var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = this.href,
+      opts   = 'status=1' +
+      ',width='  + width  +
+      ',height=' + height +
+      ',top='    + top    +
+      ',left='   + left;
+
+      window.open(url, 'twitter', opts);
+
+      return false;
+    });
   });
+
