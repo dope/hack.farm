99e4e5cbb2bcd72c7628e3dc731c258b63fae653
root <root@hack.farm>
2014-12-08 20:55:54 +0000

    fist commit

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+ <span class="toTop">
+   <i class="fa fa-chevron-up"></i>
+ </span>
+</div>
+
+<footer class="main-footer">
+  <div class="social-area">
+    <a class="popup social-area-links" href="http://www.facebook.com/share.php?u=http://hack.farm&title=hack.farm &bullet; A Hacker News Alternative"><i class="fa fa-facebook"></i></a>
+    <a class="popup social-area-links" href="http://twitter.com/home?status=hack.farm &bullet; A Hacker News Alternative+http://hack.farm"><i class="fa fa-twitter"></i></a>
+    <a class="popup social-area-links" href="https://plus.google.com/share?url=http://hack.farm"><i class="fa fa-google-plus"></i></a>
+  </div>
+ <p>Created by <a href="http://lifes.gd">Life's Good</a></p>
+</footer>
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="js/scripts.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51115801-1', 'hack.farm');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, minimal-ui">
+  <meta name="apple-mobile-web-app-title" content="hack.farm">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+  <meta name="description" content="hack.farm is a clean, responsive and elegant alternative to Hacker News created by Life's Good.">
+
+  <title>hack.farm &bullet; A Hacker News Alternative</title>
+  <link rel="stylesheet" href="css/main.css">
+
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/icon-144.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/icon-114.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/icon-72.png">
+  <link rel="apple-touch-icon-precomposed" href="img/icon.png">
+  <link rel="SHORTCUT ICON" href="img/icon.png">
+
+  <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<nav class="nav">
+  <i class="fa fa-times nav-btn nav-away"></i>
+  <ul>
+    <h4>The Nav</h4>
+    <li><a href="http://github.com/dope/hack.farm">View Source</a></li>
+    <li><a href="https://news.ycombinator.com/">Hacker News</a></li>
+    <li><a href="http://twitter.com/joericho">Created by Joe</a></li>
+  </ul>
+  <a href="http://lifes.gd" class="light-btn">Life's Good</a>
+</nav>
+
+<header class="head-wrap">
+  <i class="fa fa-bars nav-btn"></i>
+  <div class="main-nav">
+    <h1>hack.farm</h1>
+    <h3>A Hacker News Alternative</h3>
+  </div>
+</header>
+<div class="main-wrapper">
